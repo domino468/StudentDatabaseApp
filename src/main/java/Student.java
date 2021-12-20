fd64e200b75e7a7f2b5ba65df0e4466943f0a566
domino468 <dominik.adam4538@gmail.com>
2021-12-20 11:25:48 +0100

    refactoring code and adding student Id generator

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -5,24 +5,32 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
+    private String  studentID;
     private String courses;
     private String tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     public Student() {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter student first name");
+        System.out.println("Enter student first name:");
 
         this.firstName = in.nextLine();
-        System.out.println("Enter student last name");
+        System.out.println("Enter student last name:");
         this.lastName = in.nextLine();
 
         System.out.println("1 - Freshmen\n2 - Sophmore\n3 - for Junior\n4 - for Senior\nEnter student class level:");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+        setStudentId();
 
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
+    }
+    private void setStudentId(){
+        id++;
+        this.studentID =  gradeYear + " " + id;
 
     }
 }
